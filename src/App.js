3b347eeaa04e02595292902e44dad2211462a7ef
soukainahikma <soukainahikma@gmail.com>
2022-02-09 14:17:41 +0100

    display modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import "./App.css"
+import React, { useState } from 'react';
 import Buttons from "./buttons";
 function App() {
-	var listen =[
-		{data_key:"81",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3")},
-		{data_key:" ",name:new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3")}
-	]
-	function keypress(event){
-		if(event.charCode === 49)
-			listen[2].name.play()
+	var listen ={
+		"Q":["Heater-2","https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"],
+		"W":["Heater-1","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"],
+		"E":["Heater-3","https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"],
+		"A":["Heater-4","https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"],
+		"S":["Heater-6","https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"],
+		"D":["Dsc_Oh","https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"],
+		"Z":["Heater","https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"],
+		"X":["Kick_n_Hat","https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"],
+		"C":["Cev_H2","https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"]
 	}
-	document.body.addEventListener("keypress",keypress)
+	const [name,setMusic]=useState("Music")
+	// function keypress(event){
+	// 		var music = new Audio(listen[event.key.toUpperCase()][1]);
+	// 		setMusic(listen[event.key.toUpperCase()][0])
+	// 		music.play();
+	// }
+	// document.body.addEventListener("keypress",keypress)
 	return (
 		<div id="drum-machine" style={{ "marginTop": "50%" }}>
-				<div id="display" className="row drum-pad text-center">Music</div>
-				<Buttons item={listen}/>
+				<div id="display" className="row drum-pad text-center">{name}</div>
+				<Buttons item={listen} func={setMusic}/>
 		</div>
 	);
 }
--- src/buttons.js
+++ src/buttons.js
@@ -1,26 +1,32 @@
 import './App.css'
 // import { useState } from 'react'
 function Buttons(props) {
-	function PlayMusic() {
-		props.item[0].name.play()
+	function keypress(e){
+		// var music = new Audio(listen[e.key.toUpperCase()][1]);
+		props.func(props.item[e.target.children[0].id][0])
+		e.target.children[0].play()
+	}
+	function PlayMusic(e) {
+		e.target.children[0].play()
+		props.func(props.item[e.target.children[0].id][0])
 	}
 	return (<div>
 		<div className="container-fluid">
-			<div className=" row">
+			<div className="row">
 				<div className="row">
-					<button id="Q"className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>Q</button>
-					<button id="W"className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>W</button>
-					<button id="E"className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>E</button>
+					<button className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="Q" className="clip" src={props.item["Q"][1]} />Q</button>
+					<button className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="W" className="clip" src={props.item["W"][1]} />W</button>
+					<button className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="E" className="clip" src={props.item["E"][1]} />E</button>
 				</div>
 				<div className="row">
-					<button id="A"className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>A</button>
-					<button id="S"className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>S</button>
-					<button id="D"className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>D</button>
+					<button className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="A" className="clip" src={props.item["A"][1]} />A</button>
+					<button className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="S" className="clip" src={props.item["S"][1]} />S</button>
+					<button className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="D" className="clip" src={props.item["D"][1]} />D</button>
 				</div>
 				<div className="row">
-					<button id="Z" className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>Z</button>
-					<button id="X" className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>X</button>
-					<button id="C" className="col-md-4 col-sm-4 drum-pad" onClick={PlayMusic}>C</button>
+					<button  className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="Z" className="clip" src={props.item["Z"][1]} />Z</button>
+					<button  className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="X" className="clip" src={props.item["X"][1]} />X</button>
+					<button  className="col-md-4 col-sm-4 drum-pad" onKeyPress={keypress} onClick={PlayMusic}><audio id="C" className="clip" src={props.item["C"][1]} />C</button>
 				</div>
 			</div>
 		</div>
